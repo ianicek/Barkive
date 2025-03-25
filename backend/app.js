1ea027dceae66469e819916daf9d4da8a2d9fa44
davidbenenhaley <156344007+davidbenenhaley@users.noreply.github.com>
2025-03-25 10:59:07 -0400

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import Dashboard from "./dashboard";
+import Dashboard from "../frontend/dashboard";
 
 function App() {
     return (
